797e7035a381d3a70647cbd4cc493b440d124161
lejlailles <il970322@gmail.com>
2022-05-17 11:24:33 +0200

    1. commit: gitignore,readme, szerkezet

---
+++ src/main/Program.java
@@ -0,0 +1,17 @@
+
+package main;
+
+/**
+ *
+ * @author vizsgaszf
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
