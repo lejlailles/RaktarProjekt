cfa176f13927fa541df98976166c38f7fad5ea79
lejlailles <il970322@gmail.com>
2022-05-17 12:36:18 +0200

    3. commit

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,17 +1,35 @@
 
 package main;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modell.Raktar;
+
+
+
 /**
  *
  * @author vizsgaszf
  */
 public class Program {
+    Raktar raktar=new Raktar();
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-       
+        try {
+            FileOutputStream file=new FileOutputStream("raktar.bin");
+            ObjectOutputStream output = new ObjectOutputStream(file);
+           
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Program.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
     }
     
 }
